a4a2847d961cd3df59407a88d10dcd9113bd0d71
Daniel9822 <dhdany64@gmail.com>
2023-09-26 18:25:51 -0400

    feat: add context

---
+++ src/context/userContex.jsx
@@ -0,0 +1,33 @@
+'use client'
+
+import { createContext, useContext, useState } from 'react';
+
+const UserContext = createContext();
+
+export function ContextUser() {
+  return useContext(UserContext);
+}
+
+export const useUserContext = () => {
+  const user = useContext(UserContext);
+  if (!user) throw new Error('algo salio mas')
+  return user
+}
+
+export function ContextProvider({ children }) {
+  const [userActual, setUserActual] = useState({});
+
+  const addUserInfo = (user) => {
+    setUserActual(user);
+  }
+
+  const deleteUser = () => {
+    setUserActual({})
+  }
+
+  return (
+    <UserContext.Provider value={{ userActual, addUserInfo, deleteUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+}
