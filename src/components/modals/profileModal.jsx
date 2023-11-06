a4058afaa6b653067c977f6227af0f61b0370905
Daniel9822 <dhdany64@gmail.com>
2023-11-06 18:32:12 -0400

    update

--- src/components/modals/profileModal.jsx
+++ src/components/modals/profileModal.jsx
@@ -3,6 +3,7 @@ import style from './modal.module.css'
 import { useUserContext } from '@/context/userContex'
 
 export default function ProfileModal({ close }) {
+  const { userActual } = useUserContext()
   const { deleteUser } = useUserContext()
   const stopPropagation = (e) => {
     e.stopPropagation()
@@ -12,9 +13,11 @@ export default function ProfileModal({ close }) {
     localStorage.removeItem('user')
     deleteUser()
   }
+
   return (
     <ParentModal close={close}>
       <div onClick={stopPropagation} className={style.container}>
+        <span>{userActual?.name}</span>
         <button onClick={handleDeleteSession}>Cerrar session</button>
       </div>
     </ParentModal>
