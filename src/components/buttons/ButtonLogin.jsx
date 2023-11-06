8fcfed599418e3a65aa5f1044aebf15fd3e3a3f0
Daniel9822 <dhdany64@gmail.com>
2023-11-06 18:31:21 -0400

    uodate

--- src/components/buttons/ButtonLogin.jsx
+++ src/components/buttons/ButtonLogin.jsx
@@ -4,21 +4,39 @@ import style from "./button.module.css";
 import { useUserContext } from "@/context/userContex";
 import { IoIosArrowDown } from 'react-icons/io'
 import ImageProfile from "../imageProfile/ImageProfile";
+import { useEffect } from "react";
+
+const getUserActual = ({ email }) => {
+  if (!email) return
+  fetch(`http://localhost:3001/user/${email}`)
+    .then(res => res.json())
+    .then(res => res)
+    .catch(err => {
+      console.log(err)
+      return null
+    }
+    )
+}
 
 export default function ButtonLogin() {
-  const { userActual } = useUserContext();
-  const user = JSON.parse(localStorage.getItem("user"));
+  const { userActual, addUserInfo } = useUserContext();
+  const userLocal = JSON.parse(localStorage.getItem("user"));
 
-  console.log(user);
-  console.log(userActual);
+  useEffect(() => {
+    if (userLocal?.email) {
+      getUserActual({ email: userLocal.email }).then(res => {
+        addUserInfo(res)
+      })
+    }
+  }, [])
 
   return (
     <>
-      {!user?.id && !userActual?.id ? (
+      {!userActual?.id && !userActual?.id ? (
         <Link href={"http://localhost:3001/auth/google"}>Iniciar session</Link>
       ) : (
         <div className={style.imageContainer}>
-          <ImageProfile src={user.profile} />
+          <ImageProfile src={userActual.profile} />
 
           <span>{<IoIosArrowDown size={20} />}</span>
         </div>
