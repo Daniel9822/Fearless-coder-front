93aa62b30a2fe6641eb810f4947d920c710c2b05
Daniel9822 <dhdany64@gmail.com>
2023-11-06 21:55:25 -0400

    pdate create content

--- src/components/contentCreate/ContentCreate.jsx
+++ src/components/contentCreate/ContentCreate.jsx
@@ -6,29 +6,54 @@ const inter = Inter({
   subsets: ['latin']
 })
 
-export default function ContentCreate({ clipath, backgroundImg, color, background, reverse, identificate, title, paragraph, img, children }) {
+export default function ContentCreate({
+  clipath,
+  backgroundImg,
+  color,
+  background,
+  reverse,
+  identificate,
+  title,
+  paragraph,
+  img,
+  children
+}) {
   const sectionStyle = {
     background: `linear-gradient(to bottom, transparent, #0009), url(${backgroundImg})`,
     backgroundSize: 'cover',
     backgroundRepeat: 'no-repeat',
     backgroundPosition: 'center',
-    height: '100vh'
-    // clipPath: `${clipath}`
+    height: '100vh',
+    clipPath: `${clipath}`
   }
   return (
-
-    <section style={sectionStyle} id={identificate || ''} className={style.sectionMoreInfo}>
+    <section
+      style={sectionStyle}
+      id={identificate || ''}
+      className={style.sectionMoreInfo}
+    >
       <h3 className={style.titleInfo}>{title}</h3>
 
-      <div className={`${style.question} ${reverse && style.reverseItem} ${inter.className}`}>
+      <div
+        className={`${style.question} ${reverse && style.reverseItem} ${
+          inter.className
+        }`}
+      >
         <div>
-          {paragraph?.map(p => <><p style={{ color }} key={p}>{p}</p> <br /></>)}
+          {paragraph?.map((p) => (
+            <>
+              <p style={{ color }} key={p}>
+                {p}
+              </p>{' '}
+              <br />
+            </>
+          ))}
         </div>
 
-        <Image width={400} height={400} src={img} alt="interview picture" />
+        <Image width={400} height={400} src={img} alt='interview picture' />
       </div>
 
       {children}
-    </section >
+    </section>
   )
 }
