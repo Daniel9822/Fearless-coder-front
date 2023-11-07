e92abbcfc1eb6979ce22a90aae82d29741469f90
Daniel9822 <dhdany64@gmail.com>
2023-11-06 21:56:49 -0400

    update constant

--- src/constants/constansHome.js
+++ src/constants/constansHome.js
@@ -1,7 +1,7 @@
 export const titleQuestion = "¿Que es Fearless Coder?"
 export const welcomeText = `Vence tus miedos a las entrevistas de programación con
 FearlessCoder. Únete a nuestra comunidad para prepararte y triunfar
-en tus desafíos técnicos`
+en tus desafíos técnicos `
 
 export const welcomeTextTwo = `¡Sé un programador sin miedo y alcanza tu éxito en la
 programación!`
@@ -10,7 +10,7 @@ export const moreInfo = "Mas informacion"
 
 export const paragraphOne = `FearlessCoder es una plataforma diseñada para empoderar a programadores juniors y ayudarles a superar el temor a las entrevistas técnicas. Nuestro enfoque único conecta a programadores en desarrollo con experimentados reclutadores de tecnología en un proceso sencillo. Todo lo que necesitas hacer es proporcionar algunos detalles sobre tus habilidades y objetivos profesionales, y nuestra aplicación se encargará de encontrar el emparejamiento perfecto para una entrevista de práctica.`
 
-export const paragraphOneContinue = `A través de esta valiosa interacción, ganarás confianza, recibirás retroalimentación valiosa y te prepararás para destacar en tus futuras entrevistas. En FearlessCoder, creemos en tu potencial y estamos aquí para ayudarte a convertirte en un coder sin miedo, listo para triunfar en tu carrera en tecnología.`
+export const paragraphOneContinue = `A través de esta valiosa interacción, ganarás confianza, recibirás retroalimentación valiosa y te prepararás para destacar en tus futuras entrevistas.`
 
 export const titleTwo = "¿Como puedo empezar?"
 
