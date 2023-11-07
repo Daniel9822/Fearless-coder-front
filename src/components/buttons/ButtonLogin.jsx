3e2340dbde7c04c7104cddc3ca4abe9afe6db823
Daniel9822 <dhdany64@gmail.com>
2023-11-06 21:55:06 -0400

    update button login

--- src/components/buttons/ButtonLogin.jsx
+++ src/components/buttons/ButtonLogin.jsx
@@ -1,46 +1,21 @@
-"use client";
-import Link from "next/link";
-import style from "./button.module.css";
-import { useUserContext } from "@/context/userContex";
-import { IoIosArrowDown } from 'react-icons/io'
-import ImageProfile from "../imageProfile/ImageProfile";
-import { useEffect } from "react";
+'client'
 
-const getUserActual = ({ email }) => {
-  if (!email) return
-  fetch(`http://localhost:3001/user/${email}`)
-    .then(res => res.json())
-    .then(res => res)
-    .catch(err => {
-      console.log(err)
-      return null
-    }
-    )
-}
+import { useState } from 'react'
+import LoginModal from '../modals/LoginModal'
 
 export default function ButtonLogin() {
-  const { userActual, addUserInfo } = useUserContext();
-  const userLocal = JSON.parse(localStorage.getItem("user"));
+  const [openModal, setOpenModal] = useState(false)
 
-  useEffect(() => {
-    if (userLocal?.email) {
-      getUserActual({ email: userLocal.email }).then(res => {
-        addUserInfo(res)
-      })
-    }
-  }, [])
+  const handleOpenModal = () => {
+    setOpenModal(!openModal)
+  }
 
   return (
     <>
-      {!userActual?.id && !userActual?.id ? (
-        <Link href={"http://localhost:3001/auth/google"}>Iniciar session</Link>
-      ) : (
-        <div className={style.imageContainer}>
-          <ImageProfile src={userActual.profile} />
-
-          <span>{<IoIosArrowDown size={20} />}</span>
-        </div>
-      )}
+      <button onClick={handleOpenModal}>Iniciar session</button>
+      {
+        openModal && <LoginModal close={handleOpenModal}/>
+      }
     </>
-  );
+  )
 }
