2bb48c252e04d1d7b3defae069018d764dc8543c
Daniel9822 <dhdany64@gmail.com>
2023-11-08 23:08:02 -0400

    update

--- src/app/prueba/page.jsx
+++ src/app/prueba/page.jsx
@@ -1,10 +1,19 @@
 'use client'
 
-import { useUserContext } from "@/context/userContex"
+import { useUserContext } from '@/context/userContex'
+import style from './rooms.module.css'
+import Image from 'next/image'
+import UserInfo from '@/components/userInfo/UserInfo'
 
 export default function page() {
   const { userActual } = useUserContext()
-  console.log(userActual);
 
-  return <div>page</div>
+  return (
+    <section className={style.wrapper}>
+      <UserInfo userActual={userActual}/>
+      <div className={style.two}></div>
+      <div className={style.tree}></div>
+      <div className={style.four}></div>
+    </section>
+  )
 }
