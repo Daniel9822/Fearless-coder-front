5cd9e19e31a8d1443b7dc7b7e8dc1eca121cbf7e
Daniel9822 <dhdany64@gmail.com>
2023-11-09 21:45:08 -0400

    update

--- src/app/prueba/page.jsx
+++ src/app/prueba/page.jsx
@@ -2,7 +2,6 @@
 
 import { useUserContext } from '@/context/userContex'
 import style from './rooms.module.css'
-import Image from 'next/image'
 import UserInfo from '@/components/userInfo/UserInfo'
 
 export default function page() {
@@ -10,7 +9,7 @@ export default function page() {
 
   return (
     <section className={style.wrapper}>
-      <UserInfo userActual={userActual}/>
+      <UserInfo userActual={userActual} />
       <div className={style.two}></div>
       <div className={style.tree}></div>
       <div className={style.four}></div>
